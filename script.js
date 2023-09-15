3cafe144e414cdf703eea5c8c92c5a745fe29d8e
ValeriaBodrova <110822668+ValeriaBodrova@users.noreply.github.com>
2023-09-15 16:27:41 +0200

    wrote script

---
+++ script.js
@@ -0,0 +1,29 @@
+function Student(name, lastName, yearOfBirth, marks){
+    this.name = name;
+    this.lastName = lastName;
+    this.yearOfBirth = yearOfBirth;
+    this.marks = marks;
+
+    this.showAge = function(){
+        let countAge = 2023 - this.yearOfBirth;
+        return countAge;
+    }
+
+    this.showAverageScore = function(){
+
+        let sum = 0;
+        let count = 0;
+
+        for (let i = 0; i < this.marks.length; i++) {
+        sum += i;
+        count++;
+    }
+         let average = sum / count;
+
+        return average;
+    }
+
+   
+
+    const studentIvan = new Student('Ivan', 'Budko', '1999', [89, 90, 78, 96])
+console.log(studentIvan.name+''+studentIvan.lastName+''+studentIvan.yearOfBirth+''+studentIvan.marks+''+studentIvan.showAge()+''+studentIvan.showAverageScore());
