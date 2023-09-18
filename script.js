63c8a1bd605953efc6f026984b76a76369cea3d2
ValeriaBodrova <110822668+ValeriaBodrova@users.noreply.github.com>
2023-09-18 09:14:21 +0200

    hw 14 done

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-function Student(name, lastName, yearOfBirth, marks){
+function Student(name, lastName, yearOfBirth, marks = []){
     this.name = name;
     this.lastName = lastName;
     this.yearOfBirth = yearOfBirth;
     this.marks = marks;
+    this.attendance = new Array(25).fill(null);
+    this.currentIndex = 0;
 
     this.showAge = function(){
         let countAge = 2023 - this.yearOfBirth;
@@ -22,8 +24,79 @@ function Student(name, lastName, yearOfBirth, marks){
 
         return average;
     }
+   
 
+    this.present = function() {
+        if (this.currentIndex < 25) {
+            this.attendance[this.currentIndex++] = true;
+        } else {
+            console.log("Масив відвідуваності повний!");
+        }
+    }
+
+    this.absent = function() {
+        if (this.currentIndex < 25) {
+            this.attendance[this.currentIndex++] = false;
+        } else {
+            console.log("Масив відвідуваності повний!");
+        }
+    }
+
+    this.summary = function() {
+        const averageMark = this.showAverageScore();
+        const visitedLessons = this.attendance.filter(v => v === true).length;
+        const averageAttendance = visitedLessons / this.currentIndex;
+
+        if (averageMark > 90 && averageAttendance > 0.9) {
+            return "Молодець!";
+        } else if (averageMark <= 90 || averageAttendance <= 0.9) {
+            return "Добре, але можна краще";
+        } else {
+            return "Редиска!";
+        }
+    }
+
+    this.addToDOM = function() {
+        const tableBody = document.querySelector("#students-table tbody");
+        const row = tableBody.insertRow();
+
+        const nameCell = row.insertCell(0);
+        const surnameCell = row.insertCell(1);
+        const yearCell = row.insertCell(2);
+        const avgGradeCell = row.insertCell(3);
+        const attendanceCell = row.insertCell(4);
+        const summaryCell = row.insertCell(5);
+
+        nameCell.textContent = this.name;
+        surnameCell.textContent = this.lastName;
+        yearCell.textContent = this.yearOfBirth;
+        avgGradeCell.textContent = this.showAverageScore().toFixed(2);
+        attendanceCell.textContent = (this.attendance.filter(v => v).length / 25 * 100).toFixed(2) + '%';
+        summaryCell.textContent = this.summary();
+    }
 }
 
-    const studentIvan = new Student('Ivan', 'Budko', '1999', [89, 90, 78, 96])
-console.log(studentIvan.name+' '+studentIvan.lastName+' '+studentIvan.yearOfBirth+' '+studentIvan.marks+' '+studentIvan.showAge()+' '+studentIvan.showAverageScore());
+// Створюємо екземпляри студентів
+const student1 = new Student("Іван", "Іваненко", 2000, [89, 92, 78, 94, 87]);
+student1.present();
+student1.present();
+student1.absent();
+student1.absent();
+student1.addToDOM();
+
+const student2 = new Student("Марія", "Петрівна", 1999, [95, 97, 93, 91, 96]);
+student2.present();
+student2.present();
+student2.present();
+student2.addToDOM();
+
+
+
+
+
+
+
+
+
+
+
